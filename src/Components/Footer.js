083275736a797afbb10ef55a178d3d09b23205dd
Ajay-at-005173903299 <anurag.pandey17@gmail.com>
2023-01-30 19:27:44 +0530

    broscher download

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -61,50 +61,7 @@ export const Footer = () => {
   }, []);
   return (
     <div id="connect" className="footer-main">
-      {brochure && showdemo === true ? (
-        <div className="showdemo">
-          <div className="demofeild ">
-            <div className="demoheader">
-              <h5>Thank you for submitting form</h5>
-              <div className="closedemo">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="40"
-                  height="40"
-                  fill="currentColor"
-                  class="bi bi-x"
-                  viewBox="0 0 16 16"
-                  onClick={() => {
-                    setShowdemo(false);
-                  }}
-                >
-                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                </svg>
-              </div>
-            </div>
-              <div className="demobody">
-                <div className="btns-se demobodysec"  >
-                  {/* <h6>Download Brochure</h6> */}
-                  <a 
-                    href={brochure.url}
-                    target="_blank"
-                    className="browser-pdf"
-                    onClick={() => {
-                      setInterval(() => {
-                        setShowdemo(false);
-                        
-                      }, 2000);
-                    }}
-                  >
-                    Download Brochure
-                  </a>
-                </div>
-              </div>
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
+      
 
       <div className="container">
         <div className="footer-colms">
--- src/Components/Homesection-two.js
+++ src/Components/Homesection-two.js
@@ -1,10 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Slider from "./Buildingamenities";
 import Galleryhome from "./Galleryhome";
-
+import emailjs from "@emailjs/browser";
+import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 export const Homesectiontwo = () => {
   const [hometwo, setHometwo] = useState([]);
   const [brochure, setBrochure] = useState([]);
+  const [showdemo, setShowdemo] = useState(false);
+  const [footerdata, setFooterdata] = useState([]);
+  const [formdata, setFormdata] = useState({ name: "", email: "", phone: "" });
+  const [message, setMessage] = useState("");
+ 
+  useEffect(() => {
+    async function hometwodata() {
+      const hometwodata = await fetch(
+        "https://sanctorum.in/wp-sanctorum/wp-json/wp/v2/pages/33"
+      );
+      const sethometwonew = await hometwodata.json();
+      setBrochure(sethometwonew.acf.brochure_file);
+      //console.log(sethometwonew.acf.brochure_file);
+    }
+
+    hometwodata();
+  }, []);
+  const form = useRef();
+  const sendEmail = (e) => {
+    console.log(form.current);
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_4dj55kh",
+        "template_wikz79l",
+        form.current,
+        "PB7UwNxDxN07KH8Wn"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setMessage("✓ The email was sent successfully");
+          window.location.href =(brochure.url)
+          setTimeout(() => {
+            setMessage("");
+            setFormdata({ name: "", email: "", phone: "" });
+          }, 1000);
+        //   navigate(brochure.url)
+          setShowdemo(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   useEffect(() => {
     async function hometwodata() {
       const hometwodata = await fetch(
@@ -74,10 +121,108 @@ export const Homesectiontwo = () => {
         </div>
       </section>
       <div className="btns-se">
-        <a href={brochure.url} target="_blank" className="browser-pdf">
+        <a onClick={()=>{setShowdemo(true)}} target="_blank" className="browser-pdf">
           Download Brochure
         </a>
       </div>
+      {brochure && showdemo === true ? (
+        <div className="showdemo">
+            <div className="connect-right demofeild container">
+            <h3>{footerdata.connect_with_us}</h3>
+            <div className="closedemo">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  fill="currentColor"
+                  class="bi bi-x"
+                  viewBox="0 0 16 16"
+                  onClick={() => {
+                    setShowdemo(false);
+                  }}
+                >
+                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                </svg>
+              </div>
+            <form ref={form} onSubmit={sendEmail}>
+                
+              <ul>
+                <li>
+                  {" "}
+                  <input
+                    type="text"
+                    name="name"
+                    value={formdata.name}
+                    id="name"
+                    placeholder="Name"
+                    onChange={(e) => {
+                      setFormdata({
+                        ...formdata,
+                        [e.target.name]: e.target.value,
+                      });
+                    }}
+                    required
+                  />
+                </li>
+                <li>
+                  {" "}
+                  <input
+                    required
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    value={formdata.email}
+                    name="email"
+                    onChange={(e) => {
+                      setFormdata({
+                        ...formdata,
+                        [e.target.name]: e.target.value,
+                      });
+                    }}
+                  />
+                </li>
+              </ul>
+
+              <ul>
+                <input
+                  pattern="[1-9]{1}[0-9]{9}"
+                  required
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  placeholder="Phone"
+                  value={formdata.phone}
+                  onChange={(e) => {
+                    setFormdata({
+                      ...formdata,
+                      [e.target.name]: e.target.value,
+                    });
+                  }}
+                />
+              </ul>
+              <ul className="chkbx">
+                {" "}
+                <input
+                  type="checkbox"
+                  id="vehicle1"
+                  name="vehicle1"
+                  value=" By giving my phone number, i agree to being contacted by Phone, SMS, Whatsapp or any other chanel"
+                />
+                <label htmlFor="vehicle1">
+                  {" "}
+                  By giving my phone number, i agree to being contacted by
+                  Phone, SMS, Whatsapp or any other chanel.
+                </label>{" "}
+              </ul>
+
+              <input type="submit" value="ENQUIRE NOW" />
+              <h5>{message}</h5>
+            </form>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
 
       <Galleryhome />
     </>
